a57c78d453e07c2daadfe1744e6dc7a66a9030a0
thiery margot <margot.thiery@gmail.com>
2019-05-20 21:56:47 +0200

    menu display

--- my-app/src/views/Donut.vue
+++ my-app/src/views/Donut.vue
@@ -1,7 +1,33 @@
 <template>
-  <div class="chart mt-3 ml-3 text-xs-center">
-    <a @click="refreshSize" >Refresh Size</a>
-    <ChartDonut class="mt-3" :dataDonut="dataDonut" :width="chartWidth" :display="display"/>
+  <div >
+    <v-navigation-drawer permanent style="margin-top: 56px;" fixed width="200">
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">Display</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-list dense class="pt-0">
+        <v-list-tile v-for="item in items" :key="item.title" @click>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <div class="chart mt-3 text-xs-center" style="margin-left: 200px;">
+      <a @click="refreshSize">Refresh Size</a>
+      <ChartDonut class="mt-3" :dataDonut="dataDonut" :width="chartWidth" :display="display"/>
+    </div>
   </div>
 </template>
 <script>
@@ -23,7 +49,12 @@ export default {
         zoomable: true,
         hover: true,
         legend: true
-      }
+      },
+      items: [
+        { title: "Home", icon: "dashboard" },
+        { title: "About", icon: "question_answer" }
+      ],
+      right: null
     };
   },
   methods: {
@@ -33,3 +64,8 @@ export default {
   }
 };
 </script>
+<style>
+.main_div {
+  display: flex;
+}
+</style>
