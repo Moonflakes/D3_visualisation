6f289fb73c089632aafce6d3e6144cb8e266dc2c
thiery margot <margot.thiery@gmail.com>
2019-05-20 22:47:55 +0200

    menu ok

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,23 +1,61 @@
 <template>
-  <v-app>
-    <v-card height="56px">
-      <v-bottom-nav
-        :active.sync="bottomNav"
-        :color="color"
-        :value="true"
-        absolute
-        dark
-        shift
-        
-      >
-  <router-link :to="item.link" v-for="item in menu">
-        <v-btn dark>
-          <span>{{item.title}}</span>
-          <v-icon>{{item.icon}}</v-icon>
-        </v-btn>
+  <v-app id="inspire">
+    <v-navigation-drawer permanent fixed clipped class="grey lighten-4" app v-model="drawer">
+      <v-list dense class="grey lighten-4">
+        <template v-for="(item, i) in items">
+          <v-layout row v-if="item.heading" align-center :key="i">
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-right">
+              <v-btn small flat>edit</v-btn>
+            </v-flex>
+          </v-layout>
+          <v-divider dark v-else-if="item.divider" class="my-3" :key="i"></v-divider>
+          <v-list-tile :key="i" v-else @click>
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">{{ item.text }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar  app absolute clipped-left style="height: 54px;">
+      <!-- <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon> -->
+      <v-bottom-nav :active.sync="bottomNav" :color="color" :value="true" absolute dark shift>
+        <router-link :to="item.link" v-for="item in menu">
+          <v-btn dark>
+            <span>{{item.title}}</span>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-btn>
         </router-link>
       </v-bottom-nav>
-    </v-card>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height class="grey lighten-4">
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <router-view>
+              <!-- Route content goes here. -->
+             </router-view>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+
+    <!-- <v-card height="56px">
+      <v-bottom-nav :active.sync="bottomNav" :color="color" :value="true" absolute dark shift>
+        <router-link :to="item.link" v-for="item in menu">
+          <v-btn dark>
+            <span>{{item.title}}</span>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-btn>
+        </router-link>
+      </v-bottom-nav>
+    </v-card>-->
     <!-- <v-toolbar app>
       <v-menu :nudge-width="100">
         <template v-slot:activator="{ on }">
@@ -38,13 +76,13 @@
           </router-link>
         </v-list>
       </v-menu>
-    </v-toolbar> -->
+    </v-toolbar>-->
 
-    <v-content>
+    <!-- <v-content >
       <router-view>
-        <!-- Route content goes here. -->
+        Route content goes here.
       </router-view>
-    </v-content>
+    </v-content>-->
   </v-app>
 </template>
 
@@ -55,43 +93,66 @@ export default {
   name: "App",
   data() {
     return {
-      menu: [{
-        link: "/barChart",
-        icon: "bar_chart",
-        title: "Bar Chart"
-      },
-      {
-        link: "/lineChart",
-        icon: "show_chart",
-        title: "Line Chart"
-      },
-      {
-        link: "/chartBars",
-        icon: "bar_chart",
-        title: " Chart Bar"
-      },
-      {
-        link: "/chartLines",
-        icon: "show_chart",
-        title: " Chart Line"
-      },
-      {
-        link: "/chartDonut",
-        icon: "donut_large",
-        title: " Chart Donut"
-      }
+      menu: [
+        {
+          link: "/barChart",
+          icon: "bar_chart",
+          title: "Bar Chart"
+        },
+        {
+          link: "/lineChart",
+          icon: "show_chart",
+          title: "Line Chart"
+        },
+        {
+          link: "/chartBars",
+          icon: "bar_chart",
+          title: " Chart Bar"
+        },
+        {
+          link: "/chartLines",
+          icon: "show_chart",
+          title: " Chart Line"
+        },
+        {
+          link: "/chartDonut",
+          icon: "donut_large",
+          title: " Chart Donut"
+        }
       ],
-      bottomNav: 3
+      bottomNav: 3,
+      drawer: null,
+      items: [
+        { icon: "lightbulb_outline", text: "Notes" },
+        { icon: "touch_app", text: "Reminders" },
+        { divider: true },
+        { heading: "Labels" },
+        { icon: "add", text: "Create new label" },
+        { divider: true },
+        { icon: "archive", text: "Archive" },
+        { icon: "delete", text: "Trash" },
+        { divider: true },
+        { icon: "settings", text: "Settings" },
+        { icon: "chat_bubble", text: "Trash" },
+        { icon: "help", text: "Help" },
+        { icon: "phonelink", text: "App downloads" },
+        { icon: "keyboard", text: "Keyboard shortcuts" }
+      ]
     };
   },
   computed: {
-    color () {
+    color() {
       switch (this.bottomNav) {
-        case 0: return 'purple'
-        case 1: return 'teal'
-        case 2: return 'brown'
-        case 3: return 'indigo'
-        case 4: return 'pink'
+        case 0:
+          return "purple";
+        case 1:
+          return "teal";
+        case 2:
+          return "brown";
+        case 3:
+          return "indigo";
+        case 4:
+          return "pink";
       }
     }
   }
@@ -102,4 +163,16 @@ a {
   color: black;
   text-decoration: none;
 }
+
+#keep main .container {
+    height: 660px;
+  }
+
+  .navigation-drawer__border {
+    display: none;
+  }
+
+  .text {
+    font-weight: 400;
+  }
 </style>
--- my-app/src/views/Donut.vue
+++ my-app/src/views/Donut.vue
@@ -1,34 +1,8 @@
 <template>
-  <div >
-    <v-navigation-drawer permanent style="margin-top: 56px;" fixed width="200">
-      <v-toolbar flat>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-title class="title">Display</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-
-      <v-divider></v-divider>
-
-      <v-list dense class="pt-0">
-        <v-list-tile v-for="item in items" :key="item.title" @click>
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
-    <div class="chart mt-3 text-xs-center" style="margin-left: 200px;">
+    <div class="chart mt-1 text-xs-center" style="margin-left: 2px;">
       <a @click="refreshSize">Refresh Size</a>
-      <ChartDonut class="mt-3" :dataDonut="dataDonut" :width="chartWidth" :display="display"/>
+      <ChartDonut class="mt-13" :dataDonut="dataDonut" :width="chartWidth" :display="display"/>
     </div>
-  </div>
 </template>
 <script>
 import ChartDonut from "@/components/ChartDonut.vue";
